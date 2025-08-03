2ca2da9a9f10e012e56c3d0c7ef174c725b84210
Taras <126596816+tkdn00@users.noreply.github.com>
2025-08-03 16:13:58 +0300

    Update services.js

--- src/api/services.js
+++ src/api/services.js
@@ -3,28 +3,28 @@ import axios from "axios";
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export async function getProducts() {
-  const response = await axios.get(`${BASE_URL}/products`);
+  const response = await axios.get(`${BASE_URL}/api/products`);
   return response.data;
 }
 
 export async function getProductById(id) {
-  const response = await axios.get(`${BASE_URL}/products/${id}`);
+  const response = await axios.get(`${BASE_URL}/api/products/${id}`);
   return response.data;
 }
 
 export async function deleteProduct(id) {
-  await axios.delete(`${BASE_URL}/products/${id}`);
+  await axios.delete(`${BASE_URL}/api/products/${id}`);
 }
 
 export async function addProduct(name, price) {
-  await axios.post(`${BASE_URL}/products`, {
+  await axios.post(`${BASE_URL}/api/products`, {
     name,
     price,
   });
 }
 
 export async function patchProduct(id, name, price) {
-  await axios.patch(`${BASE_URL}/products/${id}`, {
+  await axios.patch(`${BASE_URL}/api/products/${id}`, {
     name,
     price,
   });
